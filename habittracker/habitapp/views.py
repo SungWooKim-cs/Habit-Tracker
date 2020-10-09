193422891b6dfa2e7735321362fe856d6899867d
SungWooKim-cs <72055468+SungWooKim-cs@users.noreply.github.com>
2020-10-09 17:53:09 -0500

    update login.html

--- habittracker/habitapp/views.py
+++ habittracker/habitapp/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
-def index(request):
-    return render(request, 'habitapp/index.html')
+def test_view(request):
+    data = {
+        'First Name': 'Ichon',
+        "Last Name": 'Kim'
+    }
+    return JsonResponse(data)
 
+# class TestView(APIview):
+#     def get(self, request, *args, **kwargs):
+#     data = {
+#         'First Name': 'Ichon',
+#         "Last Name": 'Kim'
+#     }
+#     return JsonResponse(data)
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the polls index.")
